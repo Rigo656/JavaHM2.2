ec5b46d6ae1aedb4e2e4833bfe666c32823c50c5
Kirill Grishin <Rigo656@mail.ru>
2022-02-03 17:49:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // входные данные
+
+        int wallet = 350;
+        int putMoney = 2300;
+
+        // логика
+        int bonus = putMoney / 100;
+        if (putMoney < 1000) {
+            bonus = 0;
+        }
+        int withoutBonus = wallet + putMoney;
+        int total = wallet + putMoney + bonus;
+        System.out.println("Ваш счёт: " + withoutBonus);
+        System.out.println("Бонусные рубли: " + bonus);
+        System.out.println("Итоговый счёт с бонусными рублями: " + total);
+    }
+}
